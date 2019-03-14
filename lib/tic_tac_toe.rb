851d134a7cc71e4f8528f936a2ebeff25a787e94
Michael McGovern <mmcgovern.wework@gmail.com>
2019-03-14 18:15:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,6 +32,10 @@ class TicTacToe
   
   def position_taken?(index)
     @board[index] != " "
+  end
+  
+  def valid_move?(index)
+    !position_taken?(index) && index.between?(0,8)
   end  
  
   def current_player
