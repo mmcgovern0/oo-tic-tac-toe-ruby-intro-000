af95b2a0f97672b5dea285a824a890d4850c9c5a
Michael McGovern <mmcgovern.wework@gmail.com>
2019-03-14 18:16:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,7 +46,18 @@ class TicTacToe
     @board.count{|token| token == "X" || token == "O"}
   end
   
-  
+   def turn
+    puts "Please enter a number (1-9):"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      token = current_player
+      move(index, token)
+    else
+      turn
+    end
+    display_board
+  end
  
 
 end
